38e29ca3e7725df6342e5f44b7b7b6fd10fc4674
Adriano Rodrigues <adrianojrodrigues12@gmail.com>
2023-01-23 14:41:40 -0300

    update User.php model

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,20 +8,39 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements ModelUUID
 {
     use HasApiTokens, HasFactory, Notifiable;
-
+    public $incrementing = false;
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
     protected $fillable = [
+        'user_type',
         'name',
         'email',
         'password',
+        'photo',
+        'last_login',
+        'email_verified_at',
+        'updated_at'
     ];
+
+    /**
+    "id": "17d7858f-c2c5-31ed-ad0c-6deb02f9fcd6",
+    "user_type": 0,
+    "name": "Gael Leon",
+    "email": "gaelleon@r7.com",
+    "login": "gaelleon",
+    "photo": null,
+    "last_login": null,
+    "email_verified_at": "2023-01-20T01:28:39.000000Z",
+    "created_at": "2023-01-20T01:28:40.000000Z",
+    "updated_at": "2023-01-20T01:28:40.000000Z",
+    "deleted_at": null
+     */
 
     /**
      * The attributes that should be hidden for serialization.
@@ -30,7 +49,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
