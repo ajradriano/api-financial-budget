2ef178c6db521ea34968e1ad342cad86957086b3
Adriano Rodrigues <adrianojrodrigues12@gmail.com>
2023-01-24 23:56:30 -0300

    Refactor Constants.php messages to english language

--- app/Utils/Constants.php
+++ app/Utils/Constants.php
@@ -3,24 +3,26 @@ namespace App\Utils;
 
 class Constants {
     // Conditions
-    const REQUIRED          = 'Campo Obrigatório;';
-    const UNIQUE            = 'Registro em Duplicidade;';
-    const MIN_LENGTH        = 'Tamanho Mínimo Obrigatório;';
-    const MAX_LENGTH        = 'Tamanho Máximo Excedido;';
+    const REQUIRED          = 'Required field;';
+    const EXISTS            = 'There is no record with the informed identifier;';
+    const UNIQUE            = 'Duplicate registration;';
+    const MIN_LENGTH        = 'Mandatory minimum size;';
+    const MAX_LENGTH        = 'Maximum exceeded size;';
 
     // Types
-    const STRING            = 'Deve ser Texto;';
-    const NUMERIC           = 'Deve ser Numérico;';
-    const DATE              = 'Deve ser uma Data (yyyy-mm-dd hh:ii:ss);';
-    const EMAIL             = 'Deve ser um email válido;';
+    const UUID              = 'Must be Uuid (xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx);';
+    const STRING            = 'Must be text;';
+    const NUMERIC           = 'It must be numerical;';
+    const DATE              = 'Must be a date (yyyy-mm-dd hh:ii:ss);';
+    const EMAIL             = 'Must be a valid email (example@domain.foo);';
 
     // Misc
     //  Success
-    const SAVE_SUCCESS      = 'Registro Salvo!';
-    const DELETE_SUCCESS    = 'Registro removido!';
+    const SAVE_SUCCESS      = 'Saved record!';
+    const DELETE_SUCCESS    = 'Registration removed!';
 
     //  Fail
-    const SAVE_FAIL         = 'Registro Não Salvo!';
-    const DELETE_FAIL       = 'Este item não existe ou não é válido!';
-    const LOGIN_FAIL        = 'Não foi possível fazer login. Verifique os dados!';
+    const SAVE_FAIL         = 'Record not saved!';
+    const DELETE_FAIL       = 'This item does not exist or is not valid!';
+    const LOGIN_FAIL        = 'It was not possible to login. Check the data!';
 }
